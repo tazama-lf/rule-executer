d5b2a37bc6e006ab52ce3b8cddc20c00fb141a3f
kawaki-san <rodney.kanjala@sybrin.com>
2023-06-22 14:30:34 +0200

    REFACTOR: create scaffold for different outcome branches

--- src/helpers/determineOutcome.ts
+++ src/helpers/determineOutcome.ts
@@ -1,26 +1,38 @@
-import { RuleConfig, RuleResult } from '@frmscoe/frms-coe-lib/lib/interfaces';
+import {
+  Band,
+  RuleConfig,
+  RuleResult,
+} from '@frmscoe/frms-coe-lib/lib/interfaces';
 
 const determineOutcome = (
   value: number,
   ruleConfig: RuleConfig,
   ruleResult: RuleResult,
 ): RuleResult => {
-  if (value || value === 0) {
-    for (const band of ruleConfig.config.bands) {
-      if (
-        (!band.lowerLimit || value >= band.lowerLimit) &&
-        (!band.upperLimit || value < band.upperLimit)
-      ) {
-        ruleResult.subRuleRef = band.subRuleRef;
-        ruleResult.result = band.outcome;
-        ruleResult.reason = band.reason;
-        break;
-      }
+  const { bands /*, parameters, case: ruleCase */ } = ruleConfig.config;
+
+  if (value != null) {
+    // branch on what you want to use to determine outcome (bands, params, case etc)
+    if (bands) {
+      ruleResult = withBands(bands, value, ruleResult);
     }
   } else
-    throw new Error(
-      'Value provided undefined, so cannot determine rule outcome',
-    );
+    throw new Error('Invalid value provided, so cannot determine rule outcome');
+  return ruleResult;
+};
+
+const withBands = (bands: Band[], value: number, ruleResult: RuleResult) => {
+  for (const band of bands) {
+    if (
+      (!band.lowerLimit || value >= band.lowerLimit) &&
+      (!band.upperLimit || value < band.upperLimit)
+    ) {
+      ruleResult.subRuleRef = band.subRuleRef;
+      ruleResult.result = band.outcome;
+      ruleResult.reason = band.reason;
+      break;
+    }
+  }
   return ruleResult;
 };
 
